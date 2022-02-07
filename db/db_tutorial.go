37972d8987f884722872007a3f76d742473fa9e5
staphaness@163.com <staphaness@163.com>
2022-02-07 23:13:26 +0800

    part 2

--- db/db_tutorial.go
+++ db/db_tutorial.go
@@ -2,12 +2,37 @@ package db
 
 import (
 	"fmt"
-	"os"
+	"strings"
 )
 
 const (
 	ExitSuccess int = 0
 )
+
+type MetaCommandResult int
+
+const (
+	MetaCommandSuccess MetaCommandResult = iota
+	MetaCommandUnrecognizedCommand
+)
+
+type StatementType int
+
+const (
+	StatementInsert StatementType = iota
+	StatementSelect
+)
+
+type PrepareResult int
+
+const (
+	PrepareSuccess PrepareResult = iota
+	PrepareUnrecognizedStatement
+)
+
+type Statement struct {
+	stype StatementType
+}
 
 type size_t uint
 type ssize_t int
@@ -23,14 +48,33 @@ func Run(argc int, argv ...string) int {
 	for {
 		printPromt()
 		readInput(inputBuffer)
-		if string((inputBuffer.buffer)[:5]) == ".exit" {
-			// fmt.Printf("command: %s\n", inputBuffer.buffer)
-			closeInputBuffer(inputBuffer)
-			os.Exit(ExitSuccess)
-		} else {
-			fmt.Printf("Unrecognized Commands: %s\n", inputBuffer.buffer)
-			break
+		// if strings.TrimSpace(string(inputBuffer.buffer))[:5] == ".exit" {
+		// 	// fmt.Printf("command: %s\n", inputBuffer.buffer)
+		// 	closeInputBuffer(inputBuffer)
+		// 	os.Exit(ExitSuccess)
+		// } else {
+		// 	fmt.Printf("Unrecognized Commands: %s\n", inputBuffer.buffer)
+		// 	break
+		// }
+		if inputBuffer.buffer[0] == '.' {
+			switch doMetaCommand(inputBuffer) {
+			case MetaCommandSuccess:
+				continue
+			case MetaCommandUnrecognizedCommand:
+				fmt.Printf("Unrecognized command '%s'\n", inputBuffer.buffer)
+				continue
+			}
 		}
+		var statement Statement
+		switch prepareStatement(inputBuffer, &statement) {
+		case PrepareSuccess:
+			break
+		case PrepareUnrecognizedStatement:
+			fmt.Printf("Unrecognized keyword at start of '%s'.\n", inputBuffer.buffer)
+			continue
+		}
+		executeCommand(&statement)
+		fmt.Println("Executed.")
 	}
 	return 0
 }
@@ -44,7 +88,7 @@ func printPromt() {
 }
 
 func readInput(inputBuffer *InputBuffer) bool {
-	n, err := fmt.Scan("%s\n", &inputBuffer.buffer)
+	n, err := fmt.Scan(&inputBuffer.buffer)
 	if err != nil {
 		fmt.Printf("Scan with err: %s\n", err)
 	}
@@ -54,4 +98,34 @@ func readInput(inputBuffer *InputBuffer) bool {
 func closeInputBuffer(inputBuffer *InputBuffer) int {
 	fmt.Println("Buffer Freed!")
 	return 0
+}
+
+func doMetaCommand(inputBuffer *InputBuffer) MetaCommandResult {
+	if strings.TrimSpace(string(inputBuffer.buffer))[:5] == ".exit" {
+		return MetaCommandSuccess
+	}
+	return MetaCommandUnrecognizedCommand
+}
+
+func prepareStatement(inputBuffer *InputBuffer, statement *Statement) PrepareResult {
+	if strings.TrimSpace(string(inputBuffer.buffer))[:6] == "insert" {
+		statement.stype = StatementInsert
+		return PrepareSuccess
+	}
+	if strings.TrimSpace(string(inputBuffer.buffer))[:6] == "select" {
+		statement.stype = StatementSelect
+		return PrepareSuccess
+	}
+	return PrepareUnrecognizedStatement
+}
+
+func executeCommand(statement *Statement) {
+	switch statement.stype {
+	case StatementInsert:
+		fmt.Printf("This is where we would do an insert.\n")
+		break
+	case StatementSelect:
+		fmt.Printf("This is where we would do a select.\n")
+		break
+	}
 }
