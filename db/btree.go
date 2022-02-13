b93f7b3a2e43ea1e1ebc5042727365c38b4795c6
staphaness@163.com <staphaness@163.com>
2022-02-13 23:42:21 +0800

    part 8 partial

--- db/btree.go
+++ db/btree.go
@@ -1,2 +1,45 @@
 package db
 
+import (
+	"unsafe"
+)
+
+type NodeType int
+
+const (
+	NodeInternal NodeType = iota
+	NodeLeaf
+)
+
+/* Common Node Header Layout */
+const (
+	NodeTypeSize         = uint32_t(unsafe.Sizeof(uint8(0)))
+	NodeTypeOffset       uint32_t = 0
+	IsRootSize            = uint32_t(unsafe.Sizeof(uint8(0)))
+	IsRootOffset         = NodeTypeSize
+	ParentPointerSize     = uint32_t(unsafe.Sizeof(uint32_t(0)))
+	ParentPointerOffset  = IsRootOffset + IsRootSize
+	CommonNodeHeaderSize = NodeTypeSize + IsRootSize + ParentPointerSize
+)
+
+/* Leaf Node Header Layout */
+const (
+	LeafNodeNumCellsSize  = uint32_t(unsafe.Sizeof(uint32_t(0)))
+	LeafNodeNumCellsOffset  = CommonNodeHeaderSize
+	LeafNodeHeaderSize  = CommonNodeHeaderSize + LeafNodeNumCellsSize
+)
+
+/* Leaf Node Body Layout */
+const (
+	LeafNodeKeySize = uint32_t(unsafe.Sizeof(uint32_t(0)))
+	LeafNodeKeyOffset uint32_t = 0
+	LeafNodeValueSize = RowSize
+	LeafNodeValueOffset = LeafNodeKeyOffset + LeafNodeKeySize
+	LeafNodeCellSize = LeafNodeKeySize + LeafNodeValueSize
+	LeafNodeSpaceForCells = PageSize - LeafNodeHeaderSize
+	LeafNodeMaxCells = LeafNodeSpaceForCells / LeafNodeCellSize
+)
+
+func (n *Node) name()  {
+	
+}
