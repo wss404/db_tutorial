fd51db53efa7840906236779c0538091b65e9488
staphaness@163.com <staphaness@163.com>
2022-02-17 23:03:58 +0800

    part 10 partial

--- db/btree.go
+++ db/btree.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"fmt"
+	"math"
+	"os"
 	"unsafe"
 )
 
@@ -59,6 +61,11 @@ const (
 	InternalNodeCellSize = InternalNodeKeySize + InternalNodeChildSize
 )
 
+const (
+	LeafNodeRightSplitCount = (LeafNodeMaxCells + 1) / 2
+	LeafNodeLeftSpiltCount = LeafNodeMaxCells + 1 - LeafNodeRightSplitCount
+)
+
 type Cell struct {
 	key   uint32_t
 	value Row
@@ -95,4 +102,72 @@ func (p *Page) printLeafNode()  {
 		key := *p.leafNodeKey(i)
 		fmt.Printf("  - %d : %d\n", i, key)
 	}
+}
+
+func (c *Cursor) leafNodeSplitAndInsert(key uint32_t, value *Row)  {
+	oldPage := c.table.pager.getPage(c.pageNum)
+	newPageNum := c.table.pager.getUnusedPageNum()
+	newPage := c.table.pager.getPage(newPageNum)
+	newPage.initializeLeafNode()
+
+	// TODO:有待改进
+	for i := int32(LeafNodeMaxCells); i >= 0; i-- {
+		var destinationPage *Page
+		destinationPage = oldPage
+		if i >= int32(LeafNodeLeftSpiltCount) {
+			destinationPage = newPage
+		}
+		indexWithinCell := uint32_t(i % int32(LeafNodeLeftSpiltCount))
+		destinationCell := destinationPage.leafNodeCell(indexWithinCell)
+
+		if i == int32(c.cellNum) {
+			value.serializeRow(unsafe.Pointer(destinationCell))
+		} else if i > int32(c.cellNum) {
+			oldPage.leafNodeCell(uint32_t(i-1)).moveTo(destinationCell)
+		} else {
+			oldPage.leafNodeCell(uint32_t(i)).moveTo(destinationCell)
+		}
+	}
+
+	*(oldPage.leafNodeNumCells()) = LeafNodeLeftSpiltCount
+	*(newPage.leafNodeNumCells()) = LeafNodeRightSplitCount
+
+	if oldPage.isRoot != 0 {
+		c.table.createNewRoot(newPageNum)
+	} else {
+		fmt.Println("Need to implement updating parent after split.")
+		os.Exit(ExitFailure)
+	}
+}
+
+func (p *Pager) getUnusedPageNum() uint32_t { return p.numPages }
+
+func (t *Table) createNewRoot(rightChildPageNum uint32_t)  {
+	root := t.pager.getPage(t.rootPageNum)
+	//rightChild := t.pager.getPage(rightChildPageNum)
+	leftChildPageNum := t.pager.getUnusedPageNum()
+	leftChild := t.pager.getPage(leftChildPageNum)
+
+	copy((*[PageSize]byte)(unsafe.Pointer(leftChild))[:],
+		(*[PageSize]byte)(unsafe.Pointer(root))[:])
+	leftChild.isRoot = 0
+
+	root.initializeInternalNode()
+	root.isRoot = 1
+	*(root.internalNodeNumKeys()) = 1
+	*(root.internalNodeChild(0)) = leftChildPageNum
+	leftChildMaxKey := leftChild.getMaxKey()
+	*(root.internalNodeKey(0)) = leftChildMaxKey
+	*(root.internalNodeRightChild()) = rightChildPageNum
+}
+
+func (p *Page) internalNodeNumKeys() *uint32_t {
+	if p.pageType == PageInternal {
+		return &(p.numCells)
+	}
+	panic("trying to get numCells from internal node")
+}
+
+func (p *Page) internalNodeRightChild()  {
+
 }
